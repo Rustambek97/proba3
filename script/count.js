ef0f8f303d32ee55b6adaeac80dd569e284d1f02
Rustambek97 <r.baltayev9997@gmail.com>
2024-12-05 15:15:29 +0500

    first commit

---
+++ script/count.js
@@ -0,0 +1,17 @@
+let plus = document.querySelectorAll('.plus')
+let minus = document.querySelectorAll('.minus')
+let count = document.querySelectorAll('.count')
+
+plus.forEach((item, num)=>{
+    item.addEventListener('click', ()=>{
+        count[num].value = parseInt(count[num].value) + 1
+    })
+})
+
+minus.forEach((item, num)=>{
+    item.addEventListener('click', ()=>{
+        if(count[num].value != 0){
+            count[num].value = parseInt(count[num].value) - 1
+        }
+    })
+})
